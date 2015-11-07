08848fa600699f7c88594cfad69cfdc89d9f4709
mcquiddy <cristian95mc@gmail.com>
2015-11-07 03:03:33 -0600

    prueba para jenkins suma

---
+++ sumas/main.cpp
@@ -0,0 +1,19 @@
+#include <QCoreApplication>
+#include "iostream"
+using namespace std;
+/**
+ * @brief main menu princial
+ * @param argc  argumento
+ * @param argv   arreglo
+ * @return
+ * dato
+ */
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+     int first=5;
+     int second=6;
+     int suma=first+second;
+     cout<<suma<<endl;
+    return a.exec();
+}
---
+++ sumas/sumas.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-11-07T03:00:02
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = sumas
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
